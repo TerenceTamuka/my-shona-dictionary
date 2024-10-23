4706703baa55729db0eee87c5b1cfc29cfaf7cd8
Terence Zengeya <terencetamuka@gmail.com>
2024-10-23 23:17:22 +0000

    Added heroku app domain to allowed hosts

--- shonadictionary/settings.py
+++ shonadictionary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-terencetamu-myshonadict-57mmy9stiw6.ws.codeinstitute-ide.net', 'my-shona-dictionary-app-ce78fcb5d522.herokuapp.com']
+ALLOWED_HOSTS = ['8000-terencetamu-myshonadict-57mmy9stiw6.ws.codeinstitute-ide.net','.herokuapp.com', 'my-shona-dictionary-app-ce78fcb5d522.herokuapp.com']
 
 
 # Application definition
